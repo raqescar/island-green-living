feaf5096ea956e6ddb7fd5992cbb5d4892ab6a20
raquel_e <raqescar@gmail.com>
2020-11-14 09:38:15 -0700

    initial commit

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function showReef() {
+  var x = document.getElementById("coralReef");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  console.log("it worked");
+}
